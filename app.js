d31f77a4aa31b8b342891854889334296a700e52
Matt Pahuta <mhpahuta@gmail.com>
2023-02-02 20:12:51 -0800

    Add save to/get from local storage functionality

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 console.log('app file connected')
+// *** Requirements:
+// addEventListener()
+// innerHTML
+// template strings
+// local storage
 
 const choreForm = document.getElementById('chore-form');
 const choreInput = document.getElementById('chore-input');
 const add = document.getElementById('add');
 const deleteAll = document.getElementById('delete-all');
-// const choresList = document.getElementById('chores-list');
+const choresList = document.getElementById('chores-list');
+
+let savedChores = JSON.parse(localStorage.getItem('chores')) || [];
 
 // load all event listeners
 loadEventListeners();
@@ -12,17 +19,15 @@ loadEventListeners();
 function loadEventListeners() {
   // DOM is loaded
   // ToDo: getChores from local storage
-
+  document.addEventListener('DOMContentLoaded', renderChores);
+  // add a chore
   choreForm.addEventListener('submit', addChore);
+  // delete a single chore
+  choresList.addEventListener('click', deleteChore);
+  // delete all chores from list
+  deleteAll.addEventListener('click', deleteAllChores);
+
 }
-
-// *** Requirements:
-// addEventListener()
-// innerHTML
-// template strings
-// local storage
-
-const chores = [];
 
 // add a chore
 function addChore(e) {
@@ -31,38 +36,45 @@ function addChore(e) {
   // handle empty input value
   if (chore === '') {
     alert('Add a chore') // replace alert with hidden error span?
-    return; // keep from creating a li
+    return; // keep from creating a li ***note: check if needed
   }
 
-  // prevent dup chores (stretch goal req)
-  if (!chores.includes(chore)) {
-    // add chore to choresArray? or set to localStorage?
-    chores.push(chore) 
-    renderChores(chores) // call the renderChores functions
-  } else {
-    console.log('no dup chores!') // debug - add error handling msg
-  }
-
-  // const li = document.createElement('li');
-  // li.className = 'chore';
-  // li.textContent = chore;
-  // choresList.appendChild(li)
-
-  // clear input
+  storeChoreInLocalStorage(chore)
+  renderChores()
   choreInput.value = '';
+}
+
+// add chore to local storage
+function storeChoreInLocalStorage(chore) {
+
+  savedChores.push(chore)
+  localStorage.setItem('chores', JSON.stringify(savedChores));
 
 }
 
-// render the chores from the chores array / localStorage
-function renderChores(arrayOfChores) {
+// delete a single chore
+function deleteChore(e) {
+  console.log(`The event targe is: ${e.target.textContent}`)
+}
 
-  const choresList = document.getElementById('chores-list');
+// delete all chores from list
+function deleteAllChores() {
+  localStorage.clear(); // this is too broad - need to target savedChores
+  choresList.innerHTML = ``; // call the render func instead?
+}
+
+
+// render the chores from the chores array / localStorage
+function renderChores() {
+  
+  const chores = localStorage.getItem('chores')
+  console.log(chores)
+
   let choresHtml = ''
-  for (let chore of arrayOfChores) {
+  for (let chore of savedChores) {
     choresHtml += `<li class="chore">${chore}</li>`
   }
   choresList.innerHTML = choresHtml;
 }
 
-// ToDo: add click listener for all chore li's
 
