4cd51de2fec327d16db96d98b339a822e6260a28
Matt Pahuta <mhpahuta@gmail.com>
2023-02-03 14:12:53 -0800

    Update localStorage handling funcs, streamline variable use

--- app.js
+++ app.js
@@ -1,74 +1,75 @@
-console.log('app file connected')
-// *** Requirements:
-// addEventListener()
-// innerHTML
-// template strings
-// local storage
-
+// Global variables
 const choreForm = document.getElementById('chore-form');
-const choreInput = document.getElementById('chore-input');
-const add = document.getElementById('add');
 const deleteAll = document.getElementById('delete-all');
 const choresList = document.getElementById('chores-list');
-
-let savedChores = JSON.parse(localStorage.getItem('chores')) || [];
+// savedChores - get for localStorage if it's there, otherwise it's an empty array
+const savedChores = JSON.parse(localStorage.getItem('chores')) || [];
 
 // load all event listeners
 loadEventListeners();
 
 function loadEventListeners() {
-  // DOM is loaded
-  // ToDo: getChores from local storage
+  // DOM is loaded, get render savedChores from LS
   document.addEventListener('DOMContentLoaded', renderChores);
-  // add a chore
+  // add a chore - form/button submit
   choreForm.addEventListener('submit', addChore);
   // delete a single chore
   choresList.addEventListener('click', deleteChore);
   // delete all chores from list
   deleteAll.addEventListener('click', deleteAllChores);
-
 }
 
 // add a chore
 function addChore(e) {
   e.preventDefault();
-  let chore = choreInput.value.toLowerCase(); // make everything lowerCase?
+  const choreInput = document.getElementById('chore-input');
+  const chore = choreInput.value.toLowerCase(); // make everything lowerCase?
   // handle empty input value
   if (chore === '') {
     alert('Add a chore') // replace alert with hidden error span?
-    return; // keep from creating a li ***note: check if needed
+    return; // keep from creating a li
   }
-
-  storeChoreInLocalStorage(chore)
-  renderChores()
-  choreInput.value = '';
+  // handle duplicate chore input
+  if (!savedChores.includes(chore)){
+    storeChoreInLocalStorage(chore)
+    renderChores()
+  }
+  choreInput.value = ''; // clear chore input
 }
 
 // add chore to local storage
 function storeChoreInLocalStorage(chore) {
-
   savedChores.push(chore)
   localStorage.setItem('chores', JSON.stringify(savedChores));
-
 }
 
 // delete a single chore
 function deleteChore(e) {
-  console.log(`The event targe is: ${e.target.textContent}`)
+  const choreLi = e.target; // li element of chore
+  const clickedChore = e.target.textContent; // text of chore
+  // use forEach to loop through savedChores, match up LI textContent
+  savedChores.forEach((chore, idx) => { // loop through chores to find clicked chore
+    if (clickedChore === chore) {
+      savedChores.splice(idx, 1); // remove chore with splice
+    }
+  });
+  choreLi.remove(); // remove the li element from the DOM
+  localStorage.setItem('chores', JSON.stringify(savedChores)); // set updated chores in LS
 }
 
 // delete all chores from list
 function deleteAllChores() {
-  localStorage.clear(); // this is too broad - need to target savedChores
-  choresList.innerHTML = ``; // call the render func instead?
+  // while there are chores in the savedChores array
+  while (savedChores.length > 0) {
+    savedChores.pop(); // remove any chores left in array
+  }
+  choresList.innerHTML = ``; // clear any elements in the choresList UL
+  localStorage.setItem('chores', JSON.stringify(savedChores)); // set updated chores in LS
 }
-
 
 // render the chores from the chores array / localStorage
 function renderChores() {
-  
-  const chores = localStorage.getItem('chores')
-  console.log(chores)
+  console.log(JSON.parse(localStorage.getItem('chores'))) // debug
 
   let choresHtml = ''
   for (let chore of savedChores) {
